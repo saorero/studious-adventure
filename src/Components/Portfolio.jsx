60f3b28a4780dfa001f85c6dc3e4dd61af4876fa
saorero <91612002+saorero@users.noreply.github.com>
2023-06-09 09:52:06 +0000

    changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -78,7 +78,8 @@ const Portfolio = () => {
         <div style={{ maxWidth: "40%", alignSelf: "center" }}>
           <img
             src={image}
-            style={{ height: "90%", width: "100%", objectFit: "cover" }}
+            style={{ height: "90%", width: "100%", objectFit: "cover", animation: "slideIn 1s ease-out 0s 1"
+          }}
             alt={imageAltText}
           />
         </div>
